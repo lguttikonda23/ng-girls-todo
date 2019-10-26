f877b85e36400eb94c12b995690e050b416ec6c2
unknown <lavanya.ddev@gmail.com>
2019-10-26 15:04:18 -0400

    Lguttikonda changes to Nggirls TodoList project

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit, EventEmitter, Output  } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+
+@Component({
+  selector: 'app-todo-item',
+  template: `
+  <div class="todo-item">
+    <input type="checkbox"
+         class="todo-checkbox"
+         (click)="completeItem()"/>
+  {{ item.title }}
+  <button class="btn btn-red" (click)="removeItem()">
+      remove
+    </button>
+</div>
+  `,
+  styleUrls: ['./todo-item.component.css']
+})
+
+export class TodoItemComponent implements OnInit {
+  @Input() item: TodoItem;
+  @Output() remove: EventEmitter<TodoItem> = new EventEmitter();
+  @Output() update: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+  removeItem() {
+    this.remove.emit(this.item);
+  }
+  ngOnInit() {
+  }
+  completeItem() {
+    this.update.emit({
+      item: this.item,
+      changes: {completed: !this.item.completed}
+    });
+  }
+
+}
